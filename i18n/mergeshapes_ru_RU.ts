6ca36a6872d3b499ae5fd84263c1a33b9edf441b
Alexander Bruy <alexander.bruy@gmail.com>
2010-11-26 22:22:34 +0200

    complete Russian translation

--- i18n/mergeshapes_ru_RU.ts
+++ i18n/mergeshapes_ru_RU.ts
@@ -1,53 +1,55 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
 <context>
     <name>MergeShapes</name>
     <message>
         <location filename="mergeshapes.py" line="72"/>
         <source>Quantum GIS version detected: </source>
-        <translation type="unfinished"></translation>
+        <translation>Обнаружена Quantum GIS версии: </translation>
     </message>
     <message>
         <location filename="mergeshapes.py" line="73"/>
         <source>This version of MergeShapes requires at least QGIS version 1.0.0
 Plugin will not be enabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Эта версия MergeShapes требует наличия QGIS версии 1.0.0 или старше
+Расширение не будет загружено.</translation>
     </message>
     <message>
         <location filename="mergeshapes.py" line="104"/>
         <source>Merge multiple shapefiles to one</source>
-        <translation type="unfinished"></translation>
+        <translation>Объединение нескольких shape-файлов в один</translation>
     </message>
     <message>
         <location filename="mergeshapes.py" line="96"/>
         <source>About MergeShapes</source>
         <comment>Window title</comment>
-        <translation type="unfinished"></translation>
+        <translation>О MergeShapes</translation>
     </message>
     <message>
         <location filename="mergeshapes.py" line="98"/>
         <source>&lt;b&gt;Merge Shapes&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Merge Shapes&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="mergeshapes.py" line="101"/>
         <source>Version: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Версия: %1</translation>
     </message>
     <message>
         <location filename="mergeshapes.py" line="105"/>
         <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Разработчики:&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="mergeshapes.py" line="108"/>
         <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Сайт:&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="mergeshapes.py" line="125"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрыть</translation>
     </message>
 </context>
 <context>
@@ -55,113 +57,114 @@ Plugin will not be enabled.</source>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="14"/>
         <source>Merge shapefiles</source>
-        <translation type="unfinished"></translation>
+        <translation>Объединение shape-файлов</translation>
     </message>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="20"/>
         <source>Select by layers in the folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбрать файлы в каталоге</translation>
     </message>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="29"/>
         <source>Shapefile type</source>
-        <translation type="unfinished"></translation>
+        <translation>Тип shape-файлов</translation>
     </message>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="37"/>
         <source>Polygon</source>
-        <translation type="unfinished"></translation>
+        <translation>Полигон</translation>
     </message>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="42"/>
         <source>Line</source>
-        <translation type="unfinished"></translation>
+        <translation>Линия</translation>
     </message>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="47"/>
         <source>Point</source>
-        <translation type="unfinished"></translation>
+        <translation>Точка</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="107"/>
         <source>Input directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Исходный каталог</translation>
     </message>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="90"/>
         <source>Browse</source>
-        <translation type="unfinished"></translation>
+        <translation>Обзор</translation>
     </message>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="78"/>
         <source>Output shapefile</source>
-        <translation type="unfinished"></translation>
+        <translation>Выходной файл</translation>
     </message>
     <message>
         <location filename="mergeshapesdialogbase.ui" line="99"/>
         <source>Add result to map canvas</source>
-        <translation type="unfinished"></translation>
+        <translation>Добавить результат к карте</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="55"/>
         <source>Select directory with shapefiles to merge</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите каталог с shape-файлами</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="127"/>
         <source>No shapes found</source>
-        <translation type="unfinished"></translation>
+        <translation>Shape-файлы не найдены</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="128"/>
         <source>There are no shapefiles in this directory. Please select another one.</source>
-        <translation type="unfinished"></translation>
+        <translation>В этом каталоге нет shape-файлов. Пожалуйста выберите другой каталог.</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="98"/>
         <source>Input files</source>
-        <translation type="unfinished"></translation>
+        <translation>Исходные файлы</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="144"/>
         <source>No output file selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Не указан выходной файл</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="145"/>
         <source>Please specify output filename.</source>
-        <translation type="unfinished"></translation>
+        <translation>Пожалуйста, укажите имя выходного файла.</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="151"/>
         <source>Delete error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка удаления</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="151"/>
         <source>Can&apos;t delete file %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Не могу удалить файл: %1</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="167"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Отмена</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="188"/>
         <source>Merging</source>
-        <translation type="unfinished"></translation>
+        <translation>Слияние</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="188"/>
         <source>Error loading output shapefile:
 %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка загрузки итогового shape-файла:
+%1</translation>
     </message>
     <message>
         <location filename="mergeshapesdialog.py" line="205"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрыть</translation>
     </message>
 </context>
 </TS>
