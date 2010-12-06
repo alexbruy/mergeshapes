b03642f4097db4a6c8f9400e7d7f27a3d3f721fa
Alexander Bruy <alexander.bruy@gmail.com>
2010-12-06 19:11:27 +0200

    added french translation from Jean-Cyrille Notter. Thanks!

---
+++ i18n/mergeshapes_fr_FR.ts
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>MergeShapes</name>
+    <message>
+        <location filename="mergeshapes.py" line="72"/>
+        <source>Quantum GIS version detected: </source>
+        <translation>Version de Qgis détectée :</translation>
+    </message>
+    <message>
+        <location filename="mergeshapes.py" line="73"/>
+        <source>This version of MergeShapes requires at least QGIS version 1.0.0
+Plugin will not be enabled.</source>
+        <translation>Cette version de MergeShapes requiert au minimum QGis 1.0
+Le Plugin ne sera pas chargé.</translation>
+    </message>
+    <message>
+        <location filename="mergeshapes.py" line="104"/>
+        <source>Merge multiple shapefiles to one</source>
+        <translation>Fusionne plusieurs fichiers de type shape en un</translation>
+    </message>
+    <message>
+        <location filename="mergeshapes.py" line="96"/>
+        <source>About MergeShapes</source>
+        <comment>Window title</comment>
+        <translation>Apropos MergeShapes</translation>
+    </message>
+    <message>
+        <location filename="mergeshapes.py" line="98"/>
+        <source>&lt;b&gt;Merge Shapes&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Fusionne Shapes&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="mergeshapes.py" line="101"/>
+        <source>Version: %1</source>
+        <translation>Версія: %1</translation>
+    </message>
+    <message>
+        <location filename="mergeshapes.py" line="105"/>
+        <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Developpeur:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="mergeshapes.py" line="108"/>
+        <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Page Ouaib :&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="mergeshapes.py" line="125"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>MergeShapesDialog</name>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="14"/>
+        <source>Merge shapefiles</source>
+        <translation>Fusionne les fichiers shape</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="20"/>
+        <source>Select by layers in the folder</source>
+        <translation>Sélectionner par couche dans le dossier</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="29"/>
+        <source>Shapefile type</source>
+        <translation>Type de fichier shape</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="37"/>
+        <source>Polygon</source>
+        <translation>Polygone</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="42"/>
+        <source>Line</source>
+        <translation>Ligne et pollyligne</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="47"/>
+        <source>Point</source>
+        <translation>Point</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="107"/>
+        <source>Input directory</source>
+        <translation>Répertoire en entrée</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="90"/>
+        <source>Browse</source>
+        <translation>Parcourir</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="78"/>
+        <source>Output shapefile</source>
+        <translation>Répertoire en sortie</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialogbase.ui" line="99"/>
+        <source>Add result to map canvas</source>
+        <translation>Ajouter le résultat à la liste des couches affichées</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="55"/>
+        <source>Select directory with shapefiles to merge</source>
+        <translation>Sélectionner le répertoire avec les fichiers shape à fusionner</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="127"/>
+        <source>No shapes found</source>
+        <translation>Pas de  fichier shape trouvé</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="128"/>
+        <source>There are no shapefiles in this directory. Please select another one.</source>
+        <translation>Il n&apos;y a pas de  fichier shape dans ce répertoire. En choisir une autre SVP.</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="98"/>
+        <source>Input files</source>
+        <translation>Fichiers en entrée</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="144"/>
+        <source>No output file selected</source>
+        <translation>Pas de fichier en sortie sélectionné</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="145"/>
+        <source>Please specify output filename.</source>
+        <translation>Donner un nom au fichier en sortie SVP.</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="151"/>
+        <source>Delete error</source>
+        <translation>Erreur de destruction</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="151"/>
+        <source>Can&apos;t delete file %1</source>
+        <translation>Ne peut pas effacer le fichier: %1</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="167"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="188"/>
+        <source>Merging</source>
+        <translation>Fusion</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="188"/>
+        <source>Error loading output shapefile:
+%1</source>
+        <translation>Erreur lors du chargement du fichier shape en sortie :
+%1</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="205"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="214"/>
+        <source>Save output shapefile</source>
+        <translation>Sauvegarder le fichier shape en sortie</translation>
+    </message>
+    <message>
+        <location filename="mergeshapesdialog.py" line="231"/>
+        <source>Open input shapefile(s)</source>
+        <translation>Ouvrir le(s) fichier(s) shape en entrée</translation>
+    </message>
+</context>
+</TS>
